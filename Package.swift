094a468324292106ff42855cd84073deda8776bd
HG <hotgulabjamun@users.noreply.github.com>
2021-12-21 16:50:23 -0800

    SwiftUI

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "Sliders",
+    name: "SwiftUI-Sliders",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Sliders",
-            targets: ["Sliders"]),
+            name: "SwiftUI-Sliders",
+            targets: ["SwiftUI-Sliders"]),
     ],
     dependencies: [
         .package(url: "https://github.com/hotgulabjamun/CGExtender.git", from: "1.0.1"),
@@ -23,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Sliders",
+            name: "SwiftUI-Sliders",
             dependencies: ["CGExtender", "SwiftUI-Shapes", "SwiftUI-bez"]),
         // .testTarget(
         //     name: "SlidersTests",
